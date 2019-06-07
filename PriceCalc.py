b44a535c0f89ee4fb14fcd1a9d8ef8e7e1492ee2
AK <fakeemail@efehwehfasd.com>
2019-06-06 23:23:35 -0400

    updated quantity counter

--- PriceCalc.py
+++ PriceCalc.py
@@ -21,7 +21,7 @@ import csv
 import matplotlib.pyplot as plt
 import requests
 import pandas as pd
-from datetime import datetime
+import datetime
 from pprint import pprint
 from config import api_key
 from config import weather_key
@@ -52,7 +52,7 @@ asset_id_base = "BTC"
 asset_id_quote = "USD"
 period_id = "1DAY"
 time_start = "2017-01-01T00:00:00"
-time_end = "2017-02-01T00:00:00"
+time_end = ""
 limit = "100" #cann be max of 100000
 include_empty_items = False
 ohlcv_hist_url = f'{url_base}{url_ohlcv}{asset_id_base}/{asset_id_quote}/history?period_id={period_id}&time_start={time_start}&time_end={time_end}&limit={limit}&include_empty_items={include_empty_items}'
@@ -148,30 +148,112 @@ date.isoformat()
 item_dict = [
     {
         "item": "bitcoin",
+        "name": "Bitcoin",
         "date": "2009-01-09T20:00:00",
         "price": 0
 },{
+        "item": "bitcoin_max",
+        "name": "Bitcoin Peak",
+        "date": "2017-12-17T20:00:00",
+        "price": 19783.06
+},{
         "item": "ethereum",
+        "name": "Ethereum",
         "date": "2015-07-30T20:00:00",
         "price": 0
 },{
+        "item": "ethereum_max",
+        "name": "Ethereum Peak",
+        "date": "2018-01-13T20:00:00",
+        "price": 1432.88
+},{
         "item": "ps4",
+        "name": "PS4",
         "date": "2013-11-15T20:00:00",
         "price": 399
 },{
         "item": "pizza",
+        "name": "Bitcoin Pizza",
         "date": "2010-05-22T20:00:00",
-        "price": 30/10000
+        "price": 30
 },{
-        "item": "MacBook Pro 2012",
+        "item": "macbook",
+        "name": "MacBook Pro 2012",
         "date": "2012-07-30T20:00:00",
         "price": 1199
 },{
-        "item": "Oculus Rift",
+        "item": "oculus",
+        "name": "Oculus Rift",
         "date": "2016-03-28T20:00:00",
         "price": 599
+},{
+        "item": "fiftycent",
+        "name": "50 Cent",
+        "date": "2014-02-11T20:00:00",
+        "price": 400000
 }]
 
+item_df = pd.DataFrame(item_dict)
+item_df = item_df.set_index("item")
+item_df
 
 #%% Math Attempt
+# Price of bitcoin when PS4 Came Out
+item_date = item_df.loc["ps4"]["date"]
+item_price = item_df.loc["ps4"]["price"]
+# print(item_date)
+
+time_start = item_date
+
+ohlcv_hist_url = f'{url_base}{url_ohlcv}{asset_id_base}/{asset_id_quote}/history?period_id={period_id}&time_start={time_start}&limit=1&include_empty_items={include_empty_items}'
+
+# API Call for Crypto
+headers = {'X-CoinAPI-Key' : api_key}
+response = requests.get(ohlcv_hist_url, headers=headers).json()
+# pprint(response)
+bitcoin_value_on_item_day = response[0]['price_close']
+print(bitcoin_value_on_item_day)
+
+# Quantity of Bitcoin for Price of PS4
+bitcoin_shares = item_price / bitcoin_value_on_item_day
+print(bitcoin_shares)
+
+#%%
+# PS4 at Max Bitcoin
+item_quantity_max = ((item_df.loc["bitcoin_max"]["price"])*bitcoin_shares)/item_price
+print(item_quantity_max)
+
+#%%
+# Today
+today = datetime.datetime.now() - datetime.timedelta(hours = 8)
+today = today.isoformat()
+# print(today[:19])
+
+time_start = today[:19]
+ohlcv_hist_url = f'{url_base}{url_ohlcv}{asset_id_base}/{asset_id_quote}/history?period_id={period_id}&time_start={time_start}&limit=1&include_empty_items={include_empty_items}'
+# print(ohlcv_hist_url)
+headers = {'X-CoinAPI-Key' : api_key}
+response = requests.get(ohlcv_hist_url, headers=headers).json()
+# pprint(response)
+
+bitcoin_today = response[0]['price_close']
+# print(bitcoin_today)
+
+item_quantity_current = (bitcoin_today*bitcoin_shares)/item_price
+print(item_quantity_current)
+
+
+# #%%
+# item_quantity_dict = {
+#         "item_initial_date": item_date,
+#         "item_initial_value_in_bitcoin": bitcoin_value_on_item_day,
+#         "item_initial_quantity": "",
+#         "item_max_date": "",
+#         "item_max_value_in_bitcoin": bitcoin_value_on_item_day,
+#         "item_max_quantity": item_quantity_max,
+#         "item_current_date": item_date,
+#         "item_current_value_in_bitcoin": bitcoin_today,
+#         "item_current_quantity": item_quantity_max,
+# }
+
 
--- app.py
+++ app.py
@@ -9,6 +9,12 @@ from sqlalchemy import func
 from flask_sqlalchemy import SQLAlchemy
 import price_calc_flask
 
+# For ItemQuantity
+import requests
+import pandas as pd
+import datetime
+from config import api_key
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db/coins.sqlite"
@@ -69,6 +75,108 @@ def send():
 #     }
 
 #     return jsonify(trace)
+item_dict = [
+    {
+        "item": "bitcoin",
+        "name": "Bitcoin",
+        "date": "2009-01-09T20:00:00",
+        "price": 0
+},{
+        "item": "bitcoin_max",
+        "name": "Bitcoin Peak",
+        "date": "2017-12-17T20:00:00",
+        "price": 19783.06
+},{
+        "item": "ethereum",
+        "name": "Ethereum",
+        "date": "2015-07-30T20:00:00",
+        "price": 0
+},{
+        "item": "ethereum_max",
+        "name": "Ethereum Peak",
+        "date": "2018-01-13T20:00:00",
+        "price": 1432.88
+},{
+        "item": "ps4",
+        "name": "PS4",
+        "date": "2013-11-15T20:00:00",
+        "price": 399
+},{
+        "item": "pizza",
+        "name": "Bitcoin Pizza",
+        "date": "2010-05-22T20:00:00",
+        "price": 30
+},{
+        "item": "macbook",
+        "name": "MacBook Pro 2012",
+        "date": "2012-07-30T20:00:00",
+        "price": 1199
+},{
+        "item": "oculus",
+        "name": "Oculus Rift",
+        "date": "2016-03-28T20:00:00",
+        "price": 599
+},{
+        "item": "fiftycent",
+        "name": "50 Cent",
+        "date": "2014-02-11T20:00:00",
+        "price": 400000
+}]
+
+item_df = pd.DataFrame(item_dict)
+item_df = item_df.set_index("item")
+item_df
+
+@app.route("/quantity")
+def get_quantity(item):
+    item_date = item_df.loc[item]["date"]
+    item_price = item_df.loc[item]["price"]
+    item_name = item_df.loc[item]["name"]
+
+    # URL Variables
+    time_start = item_date
+    url_base = "https://rest.coinapi.io/v1/"
+    url_ohlcv = "ohlcv/"
+    asset_id_base = "BTC"
+    asset_id_quote = "USD"
+    period_id = "1DAY"
+    include_empty_items = False
+
+    # URL Construction
+    ohlcv_hist_url = f'{url_base}{url_ohlcv}{asset_id_base}/{asset_id_quote}/history?period_id={period_id}&time_start={time_start}&limit=1&include_empty_items={include_empty_items}'
+
+    # API Call for Crypto
+    headers = {'X-CoinAPI-Key' : api_key}
+    response = requests.get(ohlcv_hist_url, headers=headers).json()
+    bitcoin_value_on_item_day = response[0]['price_close']
+    print(bitcoin_value_on_item_day)
+
+    # Quantity of Bitcoin for Price of Item
+    bitcoin_shares = item_price / bitcoin_value_on_item_day
+    print(bitcoin_shares)
+
+    # ==========
+    # Item Quantity at Max Bitcoin
+    item_quantity_max = ((item_df.loc["bitcoin_max"]["price"])*bitcoin_shares)/item_price
+    print(item_quantity_max)
+
+    # ==========
+    # Item Quantity Today
+    today = datetime.datetime.now() - datetime.timedelta(hours = 8)
+    today = today.isoformat()
+
+    time_start = today[:19]
+    ohlcv_hist_url = f'{url_base}{url_ohlcv}{asset_id_base}/{asset_id_quote}/history?period_id={period_id}&time_start={time_start}&limit=1&include_empty_items={include_empty_items}'
+
+    headers = {'X-CoinAPI-Key' : api_key}
+    response = requests.get(ohlcv_hist_url, headers=headers).json()
+
+    bitcoin_today = response[0]['price_close']
+
+    item_quantity_current = (bitcoin_today*bitcoin_shares)/item_price
+    print(item_quantity_current)
+
+    return f'You would have {item_quantity_max} {item_name}s if you sold at the peak. You would have {item_quantity_current} {item_name}s if you sold at the peak.'
 
 if __name__ == "__main__":
     app.run(debug=True)
